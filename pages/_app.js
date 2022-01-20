b2f1fa67f504df7e20faa8d14c38c522c7cfc5b0
Franco <franco.n.quintana@gmail.com>
2022-01-20 11:59:31 -0300

    feat: hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import 'tailwindcss/tailwind.css'
-import { appWithTranslation } from 'next-i18next'
+import "tailwindcss/tailwind.css";
+import { appWithTranslation } from "next-i18next";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    process.env.HJID && hotjar.initialize(process.env.HJID, 6);
+  }, []);
+  return <Component {...pageProps} />;
+};
 
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp);
