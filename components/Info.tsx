5876542a48eb42efc915a50a246e196fe0045c74
Franco <franco.n.quintana@gmail.com>
2022-01-20 11:58:21 -0300

    feat: added linkedin

--- components/Info.tsx
+++ components/Info.tsx
@@ -103,6 +103,33 @@ const Info = () => {
 						</a>
 					</div>
 				</div>
+				<div className="p-1 w-full">
+					<div className="bg-gray-100 rounded flex p-2 h-full items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+							viewBox="0 0 24 24"
+						>
+							<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+							<rect x="2" y="9" width="4" height="12"></rect>
+							<circle cx="4" cy="4" r="2"></circle>
+						</svg>
+						<a
+							href="https://www.linkedin.com/in/franco-q"
+							className="title-font font-medium"
+							target="_blank"
+						>
+							https://www.linkedin.com/in/franco-q
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
